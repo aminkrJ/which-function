0010e2693bd0465791707a804b80fce13468f3ec
Amin Karaji <amin.krj@gmail.com>
2019-12-05 22:07:03 +1100

    rollback config file when nothing has changed

--- firebase.js
+++ firebase.js
@@ -8,13 +8,19 @@ module.exports = (absPath, modified) => {
   const pattern = "firebase deploy --only functions"
   const rl = readline.createInterface({input: readStream, output: writeStream})
   rl.on("line", function(line) {
-    if(line.match(pattern) && modified.length > 0){
-      const funcNames = modified.reduce((acc, f) => {
-        const basename = path.basename(f, '.js')
-        return acc === '' ? `:${basename}` : `${acc},functions:${basename}`
-      }, '')
-      const transformedLine = line.replace(pattern, `${pattern}${funcNames}`)
-      writeStream.write(`${transformedLine}\r\n`)
+    if(line.match(pattern)){
+      if(modified.length === 0) {
+        const reg = new RegExp(pattern, )
+        const transformedLine = line.replace(/firebase deploy --only functions\S*/, pattern)
+        writeStream.write(`${transformedLine}\r\n`)
+      } else {
+        const funcNames = modified.reduce((acc, f) => {
+          const basename = path.basename(f, '.js')
+          return acc === '' ? `:${basename}` : `${acc},functions:${basename}`
+        }, '')
+        const transformedLine = line.replace(pattern, `${pattern}${funcNames}`)
+        writeStream.write(`${transformedLine}\r\n`)
+      }
     } else {
       writeStream.write(`${line}\r\n`)
     }
